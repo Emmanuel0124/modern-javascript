198e7b8f621ce894cd4bc9814ea631641241a97d
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-10 12:39:37 -0500

    comment out

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /* global moment */
 
-var datetime = moment().startOf("day").fromNow();
-console.log(datetime);
+// var datetime = moment().startOf("day").fromNow();
+// console.log(datetime);
 
 // console.log(moment().startOf("day").fromNow());
 
-var endday = moment().endOf('day').fromNow();
-console.log(endday);
+// var endday = moment().endOf('day').fromNow();
+// console.log(endday);
 
 // console.log(moment().endOf('day').fromNow());
