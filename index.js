f98c572a331f2fa506419387f1d61d1b69a9d835
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-10 12:02:24 -0500

    install,implement,and test moment.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("helllo this is js");
+/* global moment */
+
+var datetime = moment().startOf("day").fromNow();
+console.log(datetime);
+
+// console.log(moment().startOf("day").fromNow());
+
+var endday = moment().endOf('day').fromNow();
+console.log(endday);
+
+// console.log(moment().endOf('day').fromNow());
